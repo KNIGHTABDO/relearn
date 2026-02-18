bdae2f4cbb5a45fe8871fa7a4c8fb4896f902f0e
KNIGHTABDO <hiba.aabida-etu@etu.univh2c.ma>
2026-02-18 15:09:49 +0000

    fix(ci): upgrade Node to 22 (Promise.withResolvers) + add polyfill safety net

--- prebuild.js
+++ prebuild.js
@@ -1,6 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
+// Polyfill Promise.withResolvers for Node < 22 (used by pdfjs-dist)
+if (typeof Promise.withResolvers === 'undefined') {
+  Promise.withResolvers = function () {
+    let resolve, reject;
+    const promise = new Promise((res, rej) => {
+      resolve = res;
+      reject = rej;
+    });
+    return { promise, resolve, reject };
+  };
+  console.log('Added Promise.withResolvers polyfill');
+}
+
 // Remove API routes (incompatible with static export)
 const apiDir = path.join(__dirname, 'src', 'app', 'api');
 if (fs.existsSync(apiDir)) {
