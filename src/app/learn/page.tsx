c61a965118792a79777088597d49d8c5137aa588
KNIGHTABDO <hiba.aabida-etu@etu.univh2c.ma>
2026-02-18 10:48:00 +0000

    feat: src/app/learn/page.tsx (Steps 2-5)

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
 import { Header } from "@/components/layout/header";
 import { SidebarDrawer } from "@/components/layout/sidebar-drawer";
 import { DocumentViewer } from "@/components/learn/document-viewer";
@@ -9,32 +10,40 @@ import { FloatingActionBar } from "@/components/learn/floating-action-bar";
 
 export default function LearnPage() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const searchParams = useSearchParams();
+  const documentId = searchParams.get("id") || undefined;
+  const [docTitle, setDocTitle] = useState("The Genetic Code and Translation");
+
+  useEffect(() => {
+    if (documentId) {
+      fetch(`/api/document/${documentId}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.title) setDocTitle(data.title);
+        })
+        .catch(() => {});
+    }
+  }, [documentId]);
 
   return (
     <div className="flex h-screen flex-col bg-white">
-      <Header
-        title="The Genetic Code and Translation"
-        onMenuClick={() => setSidebarOpen(true)}
-      />
+      <Header title={docTitle} onMenuClick={() => setSidebarOpen(true)} />
       <SidebarDrawer open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
       {/* Two-panel split */}
       <div className="flex flex-1 overflow-hidden">
         {/* Left: Document Viewer (~60%) */}
         <div className="relative flex-[3] overflow-hidden">
-          <DocumentViewer
-            title="The Genetic Code & Translation"
-            totalPages={19}
-          />
+          <DocumentViewer title={docTitle} totalPages={19} />
 
-          {/* Floating action bar - centered at bottom */}
+          {/* Floating action bar */}
           <div className="absolute bottom-6 left-1/2 z-10 -translate-x-1/2">
             <FloatingActionBar />
           </div>
         </div>
 
         {/* Right: Learning Panel (~40%) */}
-        <LearningPanel className="w-[380px] shrink-0" />
+        <LearningPanel documentId={documentId} className="w-[380px] shrink-0" />
       </div>
     </div>
   );
